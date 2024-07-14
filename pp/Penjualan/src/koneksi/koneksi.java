5411c9c7e35bc553ecc0582cff54408dcb51e2c2
chemod77 <51191713+ch3mod@users.noreply.github.com>
2024-07-14 15:41:45 +0700

    Add files via upload

---
+++ pp/Penjualan/src/koneksi/koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/penjualan";
+                String user = "root";
+                String password = ""; // Ubah sesuai dengan password database Anda
+
+                // Register Driver
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+                // Membuat koneksi
+                koneksi = DriverManager.getConnection(url, user, password);
+
+                System.out.println("Koneksi berhasil!");
+            } catch (SQLException e) {
+                System.out.println("Koneksi gagal: " + e.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
---
+++ pp/penjualan.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2024 at 10:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `KdBrg` varchar(10) NOT NULL,
+  `NamaBrg` varchar(50) DEFAULT NULL,
+  `Harga` int(11) DEFAULT NULL,
+  `JumlahBeli` int(11) DEFAULT NULL,
+  `JumlahBayar` int(11) DEFAULT NULL,
+  `Diskon` int(11) DEFAULT NULL,
+  `TotalBayar` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`KdBrg`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
